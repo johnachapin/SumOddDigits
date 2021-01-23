2ffa287d2ed966f5ef4d35af68ceb1bbff17b120
johnachapin <chapin@vt.edu>
2021-01-23 13:07:20 -0500

    Update Main.java

--- src/main/java/com/example/project/Main.java
+++ src/main/java/com/example/project/Main.java
@@ -4,6 +4,10 @@ public class Main {
 	//Insert your code here
 
 	public static void main(final String[] args) {
+		 System.out.println("Sum of odds 13: " + sumOddDigits(13));
+    		System.out.println("Sum of odds 4682222: " + sumOddDigits(4682222));
+    		System.out.println("Sum of odds 1: " + sumOddDigits(1));
+    		System.out.println("Sum of odds 1347689: " + sumOddDigits(1347689));	
 		
 	}
 
