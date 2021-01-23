b712a1f7da5fa70131288bf849254fa4b51be559
johnachapin <chapin@vt.edu>
2021-01-23 13:00:40 -0500

    Update and rename TestHello.java to TestSumOddDigits.java

--- src/test/java/com/example/project/TestSumOddDigits.java
+++ src/test/java/com/example/project/TestSumOddDigits.java
@@ -9,20 +9,23 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.*;
 
-public class TestHello {
+public class TestSumOddDigits {
 
    @Test
-   public void testHelloWorld()
+   public void testSumOddDigits()
    {
      PrintStream originalOut = System.out;
      ByteArrayOutputStream bos = new ByteArrayOutputStream();
      System.setOut(new PrintStream(bos));
 
      // action
-     Hello.main(null);
 
      // assertion
-     assertEquals("Hello world!\n", bos.toString());
+     assertEquals(4,Main.sumOddDigits(134));
+assertEquals(1,Main.sumOddDigits(1));
+assertEquals(0,Main.sumOddDigits(4682222));
+assertEquals(0,Main.sumOddDigits(2));
+assertEquals(20,Main.sumOddDigits(1347689));
 
      // undo the binding in System
      System.setOut(originalOut);
